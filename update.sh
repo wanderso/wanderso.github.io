8d619a58d93734b519b832a7fd6b17ec0ac7daa3
William Anderson <williamharrisanderson@gmail.com>
2016-06-10 11:55:41 -0700

    "COMMITMSG"

--- update.sh
+++ update.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+COMMITMSG = "Ran default update script"
+
+for i in "$@"
+do
+case $i in
+    -c=*|--commitmsg=*)
+    COMMITMSG="${i#*=}"
+    shift # past argument=value
+    ;;
+esac
+done
+
 git add --all
-git commit -m "Ran default update script"
+git commit -m \"COMMITMSG\"
 git push -u origin master
